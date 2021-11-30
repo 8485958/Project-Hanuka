84d1129a795c364f011e4f918701a849b573f15b
Yeuda Abeles <8485958@gmail.com>
2021-11-30 22:17:36 +0200

    העלאת פרויקט חנוכה

---
+++ first.js
@@ -0,0 +1,9 @@
+document.getElementById("b1").onclick = function(){
+document.getElementById("input").style.display="block"
+}
+
+document.getElementById("b3").onclick = function(){
+    document.getElementById("input").style.display="none"
+    document.getElementById("pp").style.display="block"
+}
+    
