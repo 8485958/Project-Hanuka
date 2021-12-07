1b439c0e392780519946456ee08a231e00a585b7
Yeuda Abeles <8485958@gmail.com>
2021-12-08 00:45:52 +0200

    hanuca

--- first.js
+++ first.js
@@ -1,4 +1,4 @@
-document.getElementById("b1").onclick = function(){
+/*document.getElementById("b1").onclick = function(){
 document.getElementById("input").style.display="block"
 }
 
@@ -6,4 +6,22 @@ document.getElementById("b3").onclick = function(){
     document.getElementById("input").style.display="none"
     document.getElementById("pp").style.display="block"
 }
+
+document.getElementById("b4").onclick = function(){
+    document.getElementById("mm").style.display="block"
+}*/
+var i = 0;
+var mesimot = [0,9];
+while(true)
+{
+    document.getElementById("submit").onclick = function(){
+        mesimot[i]="oo";
+    }
+    alert(mesimot[i]);
+    i++;
+    if(i>5)
+    break;
+}
+
+
     
